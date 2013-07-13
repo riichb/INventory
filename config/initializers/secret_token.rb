925a51eb31f20333f8961f2545ef4166dd5382cb
jjw1707 <jjw1707@gmail.com>
2013-07-12 20:02:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+INventory::Application.config.secret_token = '59760d3d87c532b346bf1b56ca79e232799c7f34872d25df073a36ef0ed3616a2d7d03728beddbd4539e8bf74826d03cfe732f54bb52de07b1070b1ec3801286'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+INventory::Application.config.session_store :cookie_store, key: '_INventory_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# INventory::Application.config.session_store :active_record_store
